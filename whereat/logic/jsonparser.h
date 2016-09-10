0b4c52a1ec69aede6da92099205541d98ae4729a
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2016-09-10 20:00:15 +1200

    Implemented first complete pull from model to view.

---
+++ whereat/logic/jsonparser.h
@@ -0,0 +1,17 @@
+#ifndef JSONPARSER_H
+#define JSONPARSER_H
+
+#include <QObject>
+
+class JsonParser : public QObject
+{
+    Q_OBJECT
+public:
+    explicit JsonParser(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // JSONPARSER_H
